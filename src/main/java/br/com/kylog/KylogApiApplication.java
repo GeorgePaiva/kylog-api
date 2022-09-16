e9b098b18f8a76a140a85d2a558cd1cf31fbec51
GeorgePaiva <george.paiva15@gmail.com>
2022-09-15 21:29:47 -0300

    feat: commit inital

---
+++ src/main/java/br/com/kylog/KylogApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.kylog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KylogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KylogApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/kylog/KylogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.kylog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KylogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
